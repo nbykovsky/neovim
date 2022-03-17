00373845be983c572bd524104eb766ff585650c0
Nik <nikolay.bykovsky@gmail.com>
2022-03-17 14:52:52 +1100

    commit

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -7,5 +7,5 @@ print("lspconfig is loaded")
 
 require'lspconfig'.rust_analyzer.setup{}
 --require "user.lsp.lsp-installer"
-require("user.lsp.handlers").setup()
-require "user.lsp.null-ls"
+--require("user.lsp.handlers").setup()
+--require "user.lsp.null-ls"
