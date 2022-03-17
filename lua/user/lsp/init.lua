dd98652b12ed6a0dd774294a7ef4dfdeb767722e
Nik <nikolay.bykovsky@gmail.com>
2022-03-17 14:11:44 +1100

    add debug print

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -3,6 +3,7 @@ if not status_ok then
   print("Unable to load lspconfig")
   return
 end
+print("lspconfig is loaded")
 
 require'lspconfig'.rust_alalyzer.setup{}
 --require "user.lsp.lsp-installer"
